26eadbc9cfaf028499f26f7db8f2e756c4575c72
Sahil Saini <sahilsainimani8398@gmail.com>
2025-08-14 15:34:07 +0530

    Sidebar button worling

---
+++ script.js
@@ -0,0 +1,21 @@
+const mainContent = document.getElementById("main_content");
+
+function loadScreen(file) {
+  fetch(`screens/${file}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((html) => {
+      mainContent.innerHTML = html;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadScreen("overview.html");
+});
